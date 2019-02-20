8d7d5c6d21e994277aa01cc0e8f3f7126b54564e
Magdalena Garcia Cano <magdalena.garcia@grupoesfera.com.ar>
2019-02-20 13:56:33 -0300

    Adds failing test with more tasks

--- domain/board_manager_test.go
+++ domain/board_manager_test.go
@@ -1,8 +1,8 @@
 package domain
 
 import (
-	"testing"
 	"github.com/magiconair/properties/assert"
+	"testing"
 )
 
 func TestBoardManagerAddTaskShouldAddAsTODO(t *testing.T) {
@@ -10,7 +10,22 @@ func TestBoardManagerAddTaskShouldAddAsTODO(t *testing.T) {
 
 	boardManager.AddTask("task 1", "TODO")
 
-	assert.Equal(t, len(boardManager.Tasks),1)
+	assert.Equal(t, len(boardManager.Tasks), 1)
 	assert.Equal(t, len(boardManager.Tasks["TODO"]), 1)
-	assert.Equal(t, boardManager.Tasks["TODO"][0],"task 1")
+	assert.Equal(t, boardManager.Tasks["TODO"][0], "task 1")
+}
+
+func TestBoardManagerAddTasksShouldAddAsTODOAndWIP(t *testing.T) {
+	boardManager := NewBoardManager()
+
+	boardManager.AddTask("task 1", "TODO")
+	boardManager.AddTask("task 2", "TODO")
+	boardManager.AddTask("task 3", "WIP")
+
+	assert.Equal(t, len(boardManager.Tasks), 2)
+	assert.Equal(t, len(boardManager.Tasks["TODO"]), 2)
+	assert.Equal(t, len(boardManager.Tasks["WIP"]), 1)
+	assert.Equal(t, boardManager.Tasks["TODO"][0], "task 1")
+	assert.Equal(t, boardManager.Tasks["TODO"][1], "task 2")
+	assert.Equal(t, boardManager.Tasks["WIP"][0], "task 3")
 }
