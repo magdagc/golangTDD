2711aee9a44aa3d515cade7184ef23060537daa3
Magdalena Garcia Cano <magdalena.garcia@grupoesfera.com.ar>
2019-02-20 14:01:23 -0300

    Makes test pass

--- domain/board_manager.go
+++ domain/board_manager.go
@@ -18,7 +18,7 @@ func (b *BoardManager) AddTask(task, status string) {
 
 // GetTasks returns the slice with the tasks defined for a specific status
 func (b *BoardManager) GetTasks(status string) []string {
-	return nil
+	return b.Tasks["TODO"]
 }
 
 // ChangeStatus modifies the status of a task inside the board
