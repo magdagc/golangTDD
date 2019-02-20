e933964d6f1b3f31a1fe01e4d00a775d3eae9f1c
Magdalena Garcia Cano <magdalena.garcia@grupoesfera.com.ar>
2019-02-20 14:56:51 -0300

    Adds change status test

--- domain/board_manager_test.go
+++ domain/board_manager_test.go
@@ -69,3 +69,20 @@ func TestBoardManagerGetTasksShouldGetEmptyStringSlice(t *testing.T) {
 
 	assert.Equal(t, len(doneTasks), 0)
 }
+
+func TestBoardManagerChangeStatusFromTODOToWIP(t *testing.T) {
+	boardManager := NewBoardManager()
+
+	boardManager.AddTask("task 1", "TODO")
+	boardManager.AddTask("task 2", "TODO")
+	boardManager.AddTask("task 3", "WIP")
+
+	boardManager.ChangeStatus("task 1", "TODO", "WIP")
+
+	assert.Equal(t, len(boardManager.Tasks), 2)
+	assert.Equal(t, len(boardManager.Tasks["TODO"]), 1)
+	assert.Equal(t, len(boardManager.Tasks["WIP"]), 2)
+	assert.Equal(t, boardManager.Tasks["TODO"][0], "task 2")
+	assert.Equal(t, boardManager.Tasks["WIP"][0], "task 3")
+	assert.Equal(t, boardManager.Tasks["WIP"][1], "task 1")
+}
